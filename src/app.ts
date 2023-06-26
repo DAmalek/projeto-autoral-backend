7adbd4f4a4c4d048ce5a9bd2cbce0a5699d28ab7
Daniel Abdel <danielmalek101@gmail.com>
2023-06-26 17:36:07 -0300

    fixing ts bugs

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express, { Express } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { Response, Request } from "express";
-import { connectDB, disconnectDB } from "./config/database";
+
 import authRouter from "./routes/auth.routes";
+import { handleApplicationErrors } from "./middlewares/error.middleware";
 
 const app = express();
 
@@ -11,15 +12,7 @@ app
   .use(cors())
   .use(express.json())
   .use(authRouter)
-  .get("/health", (req: Request, res: Response) => res.send("OK!"));
-
-export function init(): Promise<Express> {
-  connectDB;
-  return Promise.resolve(app);
-}
-
-export async function close(): Promise<void> {
-  await disconnectDB();
-}
+  .get("/health", (req: Request, res: Response) => res.send("OK!"))
+  .use(handleApplicationErrors);
 
 export default app;
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const prisma = new PrismaClient();
 
-prisma = new PrismaClient();
-
-export function connectDB(): void {
-  prisma = new PrismaClient();
-}
-
-export async function disconnectDB(): Promise<void> {
-  await prisma?.$disconnect();
-}
 export default prisma;
